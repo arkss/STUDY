7326d3a6aa5c6ffe8bea837c0254588fbf31ed68
arkss <rkdalstjd9@naver.com>
2022-08-25 22:01:32 +0900

    [JPA] 쿼리 메소드 기능

--- JPA/04_실전_스프링_데이터_JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ JPA/04_실전_스프링_데이터_JPA/data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,7 +1,50 @@
 package study.datajpa.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
+import javax.persistence.Entity;
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    List<Member> findUsernameByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query("select m from Member m where m.username= :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    @Modifying
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
+
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    // 공통 메서드 오버라이드
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    // JPQL + 엔티티 그래프
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    // 메서드 이름으로 쿼리에서 특히 편리하다
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findByUsername(String username);
+
+
 }
--- JPA/04_실전_스프링_데이터_JPA/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ JPA/04_실전_스프링_데이터_JPA/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -59,4 +59,26 @@ class MemberRepositoryTest {
         long deletedCount = memberRepository.count();
         assertThat(deletedCount).isEqualTo(0);
     }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        memberRepository.findByUsernameAndAgeGreaterThan("AAA", 20);
+        memberRepository.findUsernameByUsernameAndAgeGreaterThan("AAA", 20);
+    }
+
+    @Test
+    public void bulkUpdate() throws Exception {
+        // given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 21));
+        memberRepository.save(new Member("member5", 40));
+
+        // when
+        int resultCount = memberRepository.bulkAgePlus(20);
+
+        // then
+        assertThat(resultCount).isEqualTo(3);
+    }
 }
